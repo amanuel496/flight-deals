3d91d8fd04c020909d0c6c9955271b93f8c8db74
Amanuel Chukala <amanuelchukala@gmail.com>
2023-09-18 08:09:26 -0600

    Added flight search feature

--- flight_search.py
+++ flight_search.py
@@ -7,35 +7,56 @@
 import datetime
 
 import requests
+from flight_data import FlightData
+
 KIWI_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
-today = datetime.datetime.now().date()
-tomorrow = today + datetime.timedelta(days=1)
-after_six_months = today + datetime.timedelta(days=180)
-
-headers = {
-"apikey": "CbEmcSAliSbv9GmQJpgvLC0lyuiwwXvZ"
-}
-
-params = {
-    "fly_from": "DEN",
-    "fly_to": "ADD",
-    "date_from": tomorrow,
-    "date_to": after_six_months,
-    "selected_cabins": "M",
-    "adult_hold_bag": 1,
-    "adult_hand_bag": 1,
-    "curr": "USD",
-    "price_to": 1281,
-    "limit": 20
-}
-
-flight_data = requests.get(KIWI_ENDPOINT, params, headers=headers)
-flight_data.raise_for_status()
-
-print(flight_data.json())
+KIWI_API_KEY = "CbEmcSAliSbv9GmQJpgvLC0lyuiwwXvZ"
+CITY = ["ADD", "PAR", "BER", "TYO"]
+FLY_FROM = "DEN"
+CABIN = "M"
+HOLD_BAG = 1
+HAND_BAg = 1
+CURRENCY = "USD"
 
 
+class FlightSearch:
+    def __init__(self):
+        self.flight_prices = []
 
-# print(tomorrow)
+    def get_data(self):
+        today = datetime.datetime.now().date()
+        tomorrow = today + datetime.timedelta(days=1)
+        after_six_months = today + datetime.timedelta(days=180)
+
+        headers = {
+            "apikey": KIWI_API_KEY
+        }
+
+        for fly_to in CITY:
+            params = {
+                "fly_from": FLY_FROM,
+                "fly_to": fly_to,
+                "date_from": tomorrow,
+                "date_to": after_six_months,
+                "selected_cabins": CABIN,
+                "adult_hold_bag": HOLD_BAG,
+                "adult_hand_bag": HAND_BAg,
+                "curr": "USD",
+                # "price_to": 1281,
+                # "limit": 20
+            }
+
+            response = requests.get(KIWI_ENDPOINT, params, headers=headers)
+            response.raise_for_status()
+
+            city = response.json()["data"][0]["cityTo"]
+            price = response.json()["data"][0]["price"]
+
+            flight_data = FlightData(city=city, iata_code=fly_to, lowest_price=price)
+            self.flight_prices.append(flight_data)
+
+            # print(flight_data.city, " ", flight_data.iata_code, " ", flight_data.lowest_price)
+
+# fligh_search = FlightSearch()
 #
-# print(after_six_months)
+# fligh_search.get_data()
